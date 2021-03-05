2649351f91baf6b20c31cf4c2c88e69056f03836
Enrico R <enricorrx@gmail.com>
2021-03-05 22:18:30 +0100

    Added CMakeLists.txt

--- src/build-and-run.sh
+++ src/build-and-run.sh
@@ -3,7 +3,7 @@ if [ -f Makefile ]; then
   make again
   echo "+------------ EXECUTABLE ------------+"
   clear
-  ./tsp
+  ./tsp --file ../data/att532.tsp --time-limit 1000
   printf "\n\n"
 else
   echo "Cannot find Makefile"
--- src/main.c
+++ src/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
-int main(){
+#include <stdlib.h>
+#include <cplex.h>
+#include "tsp.h"
+
+// Debug functions
+void debug(const char *err) { printf("\nDEBUG: %s \n", err); }
+void print_error(const char *err) { printf("\n\n ERROR: %s \n\n", err); exit(1); }
+
+int main(int argc, char** argv){
     printf("It works!");
 }
