5a9fb900475a81209d1daa0e7b0c32756a3baa7b
Enrico R <enricorrx@gmail.com>
2021-03-08 11:10:25 +0100

    File parser verified and small changes

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Install [IMB CPLEX Optimizer](https://www.ibm.com/analytics/cplex-optimizer).
 1. Set your own `CPLEX_HEADERS` and `CLEX_LIBS` path on `CMakeLists.txt`
 2. Run `cmake .`
 3. Run `make`
-4. Run `./tsp --help`
+## How to run
+Type `./tsp --help` to see available parameters.  
+Mandatory parameters are:
+1. `--file <file>` specify tsp file path 
+2. `--time-limit <time>` specify overall time in seconds
 ## Data
 You can download tsp input files from [here](http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/tsp/).
--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #define USAGE   "Usage: ./tsp --file <input-file> --time-limit <time> [options]\n" \
                 "--file <input-file>                file in tsp format\n"\
                 "--time-limit <time>                max overall time in seconds\n" \
-                "--verbose <0,1,2,3>                0=quiet, 1=default, 2=verbose, 3=very verbose\n" \
+                "--verbose <n>                      0=quiet, 1=default, 2=verbose, 3=very verbose\n" \
                 "--help                             show this help\n\n"
 
 void free_instance(instance *inst){
@@ -26,28 +26,34 @@ void parse_command_line(int argc, char** argv, instance *inst){
         if(strcmp(argv[i], "--verbose") == 0){ inst->verbose = atoi(argv[++i]); continue;}
         if (strcmp(argv[i],"--help") == 0) { help = 1; continue; }
 
-        printf("Unknown option: %s\n", argv[i]);
+        printf("ERROR: Unknown option: %s\n", argv[i]);
         help = 1; // need to show help if came here
     }
-    if(help){
-        printf(USAGE);
-        free_instance(inst);
-        exit(1);
-    }
+
+    if(inst->verbose >=2) printf("[*] Command line arguments parsed.\n");
 
     // print arguments
-    if(inst->verbose > 1){
-        if(inst->verbose > 2){ printf(USAGE);}
+    if(inst->verbose >= 2){
+        if(inst->verbose >= 3){ printf(USAGE);}
         printf("Command line arguments found (include defaults):\n");
         printf("--file          %s\n", inst->input_file_name);
         printf("--time-limit    %f\n", inst->time_limit);
         printf("--verbose       %d\n", inst->verbose);
     }
 
-    // TODO check mandatory arguments
+    if(help){
+        printf(USAGE);
+        exit(1);
+    }
+
+    if((inst->input_file_name == NULL) || (inst->time_limit == -1)){
+        printf("ERROR: Check mandatory arguments!\n");
+        exit(1);
+    }
 }
 
 void parse_tsp_file(instance *inst){
+    if(inst->verbose >=2) printf("[*] Parsing tsp file...\n");
     // put default instance values
     inst->tot_nodes = -1;
     inst->xcoord = inst->ycoord = NULL;
@@ -65,18 +71,18 @@ void parse_tsp_file(instance *inst){
     char done = 0;
     while(!done){
         if(fgets(line, sizeof(line), fin) == NULL){
-            printf("EOF not found!\n");
+            printf("ERROR: EOF not found!\n");
             free_instance(inst);
             exit(1);
         }
-        if(inst->verbose >= 2) printf("line: %s\n", line);
-        param_name = strtok(line, ": ");
-        if(inst->verbose >= 2) printf("param_name: %s\n", param_name);
+        if(inst->verbose >= 3) printf("line: %s", line);
+        param_name = strtok(line, ": \n");
+        if(inst->verbose >= 3) printf("param_name: %s\n", param_name);
 
         if(strncmp(param_name, "NAME", 4) == 0) continue;
 
         if(strncmp(param_name, "TYPE", 4) == 0){
-            param = strtok(NULL, ": ");
+            param = strtok(NULL, ": \n");
             if(strncmp(param, "TSP",3) != 0){
                 printf("ERROR: TYPE %s not supported yet.", param);
                 free_instance(inst);
@@ -88,13 +94,13 @@ void parse_tsp_file(instance *inst){
         if(strncmp(param_name, "COMMENT", 7) == 0) continue;
 
         if(strncmp(param_name, "DIMENSION", 9) == 0){
-            param = strtok(NULL, ": ");
+            param = strtok(NULL, ": \n");
             inst->tot_nodes = atoi(param);
             continue;
         }
 
         if(strncmp(param_name, "EDGE_WEIGHT_TYPE", 16) == 0){
-            param = strtok(NULL, ": ");
+            param = strtok(NULL, ": \n");
             if(strncmp(param, "ATT", 3) != 0){
                 printf("ERROR: EDGE_WEIGHT_TYPE %s not supported yet.", param);
                 free_instance(inst);
@@ -111,14 +117,17 @@ void parse_tsp_file(instance *inst){
             }
             inst->xcoord = (double *) calloc(inst->tot_nodes, sizeof(double));
             inst->ycoord = (double *) calloc(inst->tot_nodes, sizeof(double));
+
             for(int n = 0; n < inst->tot_nodes; n++){
-                if(fgets(line, sizeof(line), fin) == NULL){
-                    printf("ERROR: Too few nodes! Expected %d, got %d\n", inst->tot_nodes, n+1);
+                if((fgets(line, sizeof(line), fin) == NULL) || (strncmp(line, "EOF", 3) == 0)){
+                    printf("ERROR: Too few nodes! Expected %d, got %d\n", inst->tot_nodes, n);
                     free_instance(inst);
                     exit(1);
                 }
                 if(atoi(strtok(line, " ")) != n+1){
-                    printf("WARN: Nodes seem not ordered!\n");
+                    printf("ERROR: Nodes must be ordered!\n");
+                    free_instance(inst);
+                    exit(1);
                 }
                 inst->xcoord[n] = atof(strtok(NULL, " "));
                 inst->ycoord[n] = atof(strtok(NULL, " "));
@@ -128,17 +137,17 @@ void parse_tsp_file(instance *inst){
 
         if(strncmp(param_name, "EOF", 3) == 0) done = 1;
     }
-
-
-
-
+    if(inst->verbose >=2) printf("[*] Tsp file parsed.\n");
+    if(inst->verbose >= 3){
+        printf("Nodes found:\n");
+        for(int i=0; i < inst->tot_nodes; i++)
+            printf("n%d = (%f, %f)\n", i+1, inst->xcoord[i], inst->ycoord[i]);
+    }
 }
-
-
 
 int main(int argc, char **argv){
     instance inst;
     parse_command_line(argc, argv, &inst);
     parse_tsp_file(&inst);
-    free_instance(&inst); // release occupied memory!
+    free_instance(&inst); // release memory!
 }
