3f694782e5b793225535d5f1c35890f21b27e6be
Enrico R <enricorrx@gmail.com>
2021-03-09 16:18:21 +0100

    Presentation improved

--- src/main.c
+++ src/main.c
@@ -12,21 +12,21 @@ void free_instance(instance *inst){
     free(inst->ycoord);
 }
 
-void parse_command_line(int argc, char** argv, instance *inst){
+void parse_command_line(int argc, char **argv, instance *inst){
     // put default instance value
     inst->input_file_name = NULL;
     inst->time_limit = -1;
     inst->verbose = 1;
 
     // parse cli
-    char help = (argc > 2) ? 0 : 1;
+    char help = (argc >= 3) ? 0 : 1;
     for(int i = 1; i < argc; i++){
         if(strcmp(argv[i],"--file") == 0){ inst->input_file_name = argv[++i]; continue;}
         if(strcmp(argv[i],"--time-limit") == 0){ inst->time_limit = atof(argv[++i]); continue;}
         if(strcmp(argv[i], "--verbose") == 0){ inst->verbose = atoi(argv[++i]); continue;}
         if (strcmp(argv[i],"--help") == 0) { help = 1; continue; }
 
-        printf("ERROR: Unknown option: %s\n", argv[i]);
+        printf(RED "ERROR: Unknown option: %s\n" RESET, argv[i]);
         help = 1; // need to show help if came here
     }
 
@@ -47,13 +47,13 @@ void parse_command_line(int argc, char** argv, instance *inst){
     }
 
     if((inst->input_file_name == NULL) || (inst->time_limit == -1)){
-        printf("ERROR: Check mandatory arguments!\n");
+        printf(RED "ERROR: Check mandatory arguments!\n" RESET);
         exit(1);
     }
 }
 
 void parse_tsp_file(instance *inst){
-    if(inst->verbose >=2) printf("[*] Parsing tsp file...\n");
+    if(inst->verbose >=2) printf("[ ] Parsing tsp file...\n");
     // put default instance values
     inst->tot_nodes = -1;
     inst->xcoord = inst->ycoord = NULL;
@@ -61,7 +61,7 @@ void parse_tsp_file(instance *inst){
     // open file
     FILE *fin = fopen(inst->input_file_name, "r");
     if (fin == NULL ){
-        printf("ERROR: input file %s not found!", inst->input_file_name);
+        printf(RED "ERROR: input file %s not found!" RESET, inst->input_file_name);
         free_instance(inst);
         exit(1);}
 
@@ -71,20 +71,20 @@ void parse_tsp_file(instance *inst){
     char done = 0;
     while(!done){
         if(fgets(line, sizeof(line), fin) == NULL){
-            printf("ERROR: EOF not found!\n");
+            printf(RED "ERROR: EOF not found!\n" RESET);
             free_instance(inst);
             exit(1);
         }
-        if(inst->verbose >= 3) printf("line: %s", line);
+        if(inst->verbose >= 3) printf("Reading line: %s", line);
         param_name = strtok(line, ": \n");
-        if(inst->verbose >= 3) printf("param_name: %s\n", param_name);
+        param = strtok(NULL, ": \n");
+        if(inst->verbose >= 3) printf("param_name= %s, param = %s\n", param_name, param);
 
         if(strncmp(param_name, "NAME", 4) == 0) continue;
 
         if(strncmp(param_name, "TYPE", 4) == 0){
-            param = strtok(NULL, ": \n");
             if(strncmp(param, "TSP",3) != 0){
-                printf("ERROR: TYPE %s not supported yet.", param);
+                printf(RED "ERROR: TYPE %s not supported yet.\n" RESET, param);
                 free_instance(inst);
                 exit(1);
             }
@@ -94,15 +94,13 @@ void parse_tsp_file(instance *inst){
         if(strncmp(param_name, "COMMENT", 7) == 0) continue;
 
         if(strncmp(param_name, "DIMENSION", 9) == 0){
-            param = strtok(NULL, ": \n");
             inst->tot_nodes = atoi(param);
             continue;
         }
 
         if(strncmp(param_name, "EDGE_WEIGHT_TYPE", 16) == 0){
-            param = strtok(NULL, ": \n");
             if(strncmp(param, "ATT", 3) != 0){
-                printf("ERROR: EDGE_WEIGHT_TYPE %s not supported yet.", param);
+                printf(RED "ERROR: EDGE_WEIGHT_TYPE %s not supported yet." RESET, param);
                 free_instance(inst);
                 exit(1);
             }
@@ -111,7 +109,7 @@ void parse_tsp_file(instance *inst){
 
         if(strncmp(param_name, "NODE_COORD_SECTION", 18) == 0) {
             if(inst->tot_nodes <= 0){
-                printf("ERROR: Can't allocate %d nodes!", inst->tot_nodes);
+                printf("ERROR: Cannot allocate %d nodes!", inst->tot_nodes);
                 free_instance(inst);
                 exit(1);
             }
@@ -120,17 +118,19 @@ void parse_tsp_file(instance *inst){
 
             for(int n = 0; n < inst->tot_nodes; n++){
                 if((fgets(line, sizeof(line), fin) == NULL) || (strncmp(line, "EOF", 3) == 0)){
-                    printf("ERROR: Too few nodes! Expected %d, got %d\n", inst->tot_nodes, n);
+                    printf(RED "ERROR: Too few nodes! Expected %d, got %d\n" RESET, inst->tot_nodes, n);
                     free_instance(inst);
                     exit(1);
                 }
+                if(inst->verbose >= 3) printf("Reading line: %s", line);
                 if(atoi(strtok(line, " ")) != n+1){
-                    printf("ERROR: Nodes must be ordered!\n");
+                    printf(RED "ERROR: Nodes must be ordered!\n" RESET);
                     free_instance(inst);
                     exit(1);
                 }
                 inst->xcoord[n] = atof(strtok(NULL, " "));
                 inst->ycoord[n] = atof(strtok(NULL, " "));
+                if(inst->verbose >=3) printf("n%d = (%f, %f)\n", n+1, inst->xcoord[n], inst->ycoord[n]);
             }
             continue;
         }
@@ -138,11 +138,6 @@ void parse_tsp_file(instance *inst){
         if(strncmp(param_name, "EOF", 3) == 0) done = 1;
     }
     if(inst->verbose >=2) printf("[*] Tsp file parsed.\n");
-    if(inst->verbose >= 3){
-        printf("Nodes found:\n");
-        for(int i=0; i < inst->tot_nodes; i++)
-            printf("n%d = (%f, %f)\n", i+1, inst->xcoord[i], inst->ycoord[i]);
-    }
     fclose(fin);
 }
 
