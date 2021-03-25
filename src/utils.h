2502c139b8acd7802d26012b4be194bb526c46ac
Enrico R <enricorrx@gmail.com>
2021-03-25 11:31:56 +0100

    added comments; added --perfl option

--- src/utils.h
+++ src/utils.h
@@ -34,7 +34,7 @@ typedef struct{
     bool do_plot;                   // use gnu-plot
     bool no_opt;                    // don't show known optimal solution
     int perfr;                      // run performance test on random points up to <max> size
-    char *perfl;                    // run performance test on a list
+    char *perfl;                    // run performance test on a list written to file
     int runs;                       // decide how many runs execute on performance test
     int verbose;                    // print level
 
